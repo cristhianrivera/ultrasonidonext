40e098ab0dbb952cc7e14b7e09725dc8cb68dc2f
CristhianRivera <cristhianr.jimenez.rivera@gmail.com>
2020-07-19 18:31:01 +0200

    no transition

--- components/Banner.js
+++ components/Banner.js
@@ -3,7 +3,7 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1><img src="/static/images/logoUM_horizontal.png" width="17%" height="17%" alt="" class="center"/> <br></br>
+                <h1><img src="/static/images/logoUM_horizontal_small.png"  alt="ultrasonido médico diagnótico en la colonia roma CDMX" class="center"/> <br></br>
                 Ultrasonido médico diagnóstico en la Ciudad de México</h1>
             </header>
 
