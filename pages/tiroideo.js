7477653e335398d763fffb43a9b243bca8f22f62
CristhianRivera <cristhianr.jimenez.rivera@gmail.com>
2020-10-04 18:33:01 +0200

    improved design on tiroide and 4d

--- pages/tiroideo.js
+++ pages/tiroideo.js
@@ -21,27 +21,37 @@ export default () => (
             </BannerLanding> 
 
             <div id="main">
-                <section id="one">
-                    <div className="inner">
-                        <h3>¿Cómo es el estudio de tiroides?</h3>
-                        <p>Es un estudio de imagen en alta resolución para examinar la forma y posición de la glándula tiroides así como las estructuras cercanas en el cuello.</p>
-
-                    </div>
-                </section>
                 <section id="two" className="spotlights">
                     <section>
-                            {/* <a className="image "><img src="/static/images/Doppler_best.jpg" alt="" /></a> */}
-                            <div className="u"><span className="image fit"><img src="/static/images/Doppler_short.jpg" alt="ultrasonido de tiroides" title='ultrasonido tiroides'/></span></div>
+                        <div className="u"><span className="image fit"><img src="/static/images/Tiroides.jpg" alt="ultrasonido de tiroides" title='ultrasonido tiroides'/></span></div>
                         <div className="content">
                             <div className="inner">
-                            <header className="minor">
+                                <header className="minor">
+                                <h3>¿Qué es un ultrasonido de tiroides?</h3>
+                                </header>
+                                <p>Es un estudio médico diágnotisco en donde se utiliza un equipo de ultrasonido de alta definición para examinar la forma y posición de la <b>glándula tiroides</b> así como las estructuras cercanas en el cuello.</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section>
+                        <div className="u"><span className="image fit"><img src="/static/images/Doppler_short.jpg" alt="ultrasonido de tiroides" title='ultrasonido tiroides'/></span></div>
+                        <div className="content">
+                            <div className="inner">
+                                <header className="minor">
                                     <h4>¿Qué puede conocer el médico con base al estudio?</h4>
-                            </header>
-                            <p>Se reportan características de normalidad y enfermedades de la glándula, desde: Bocio (crecimiento de la tiroides), nodulaciones, quistes; simples y complejos, adenomas, calcificaciones, así como sospecha de cáncer.</p>
-                            <header className="minor">
-                                <h4>¿Porqué me pueden enviar un estudio de ultrasonido de tiroides?</h4>
-                            </header>
-                                <p>Habitualmente se realizan cuando hay sospecha de función baja o alta de la tiroides(hipo ó hiper), abultamiento del cuello en su porción anterior y por síntomas que puede detectar el médico.</p>
+                                </header>
+                                    <p>En el informe del ultrasonido de tiroides se reportan características de normalidad, medidas y enfermedades de la glándula, desde: Bocio (crecimiento de la tiroides), nodulaciones, quistes; simples y complejos, adenomas, calcificaciones, así como sospecha de cáncer.</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section>
+                        {/* <div className="u"><span className="image fit"><img src="/static/images/Doppler_short.jpg" alt="ultrasonido de tiroides" title='ultrasonido tiroides'/></span></div> */}
+                        <div className="content">
+                            <div className="inner">
+                                <header className="minor">
+                                    <h4>¿Porqué me pueden enviar un estudio de ultrasonido de tiroides?</h4>
+                                </header>
+                                    <p>Habitualmente se realizan cuando hay sospecha de función baja o alta de la tiroides(hipo ó hiper), abultamiento del cuello en su porción anterior y por síntomas que puede detectar el médico.</p>
                             </div>
                         </div>
                     </section>
