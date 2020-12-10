7561c7f9a7e85dd9d3b75630ddecadd836ed4bdc
Cristhian Rivera <cristhian.jimenez.rivera@gmail.com>
2020-12-10 14:08:43 -0600

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -3,13 +3,13 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1><img src="/static/images/logoUM_horizontal_small.png"  alt="ultrasonido médico diagnótico en la colonia roma CDMX" class="center"/> <br></br>
-                Ultrasonido médico diagnóstico en la Ciudad de México</h1>
-                <div className="inner">
+                <h1><img src="/static/images/logoUM_horizontal_small.png"  alt="ultrasonido médico en CDMX" class="center"/> <br></br>
+                Ultrasonido médico general y de alta especialidad en CDMX</h1>
+                    {/*<div className="inner">
                     <p>
                     Estudios de ultrasonido médico con equipo de última generación en la Ciudad de México.
                     </p>
-                </div>
+                </div>*/}
             </header>
 
             
